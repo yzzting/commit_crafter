3c8a8f868d031c67b783321b0082a97d964b0755
yzzting <yzz1995@aliyun.com>
2024-09-02 14:29:00 +0800

    ci: Skip tests in GitHub Actions environment

--- tests/test_llm_openai.rs
+++ tests/test_llm_openai.rs
@@ -4,6 +4,10 @@ use tempfile::tempdir;
 
 #[test]
 fn test_openai_request_by_no_config() {
+    if env::var("GITHUB_ACTIONS").is_ok() {
+        eprintln!("Skipping test in GitHub Actions environment");
+        return;
+    }
     let temp_dir = tempdir().unwrap();
     let prompt_dir = temp_dir.path().join("prompt.toml");
     let file_path = temp_dir.path();
@@ -28,6 +32,10 @@ fn test_openai_request_by_no_config() {
 
 #[test]
 fn test_openai_request() {
+    if env::var("GITHUB_ACTIONS").is_ok() {
+        eprintln!("Skipping test in GitHub Actions environment");
+        return;
+    }
     let home_dir = env::var("HOME").expect("Error getting home directory");
     let config_dir = format!("{}/.config/commit_crafter", home_dir);
     let prompt_dir = format!("{}/.config/commit_crafter/prompt.toml", home_dir);
